fe69646a79d877baeef148bb8f82697668dfa435
Hayden <hdnsimpson@gmail.com>
2019-07-26 12:51:38 +1200

    Added CryptoCompare 3rd party api heler

---
+++ CryptoCognizantAPI/CryptoCognizant/CryptoCognizant/Helper/CryptoCompareHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CryptoCognizant.Model;
+
+namespace CryptoCognizant.Helper
+{
+    public class CryptoCompareHelper
+    {
+        public static void testProgram()
+        {
+            Console.WriteLine("Test");
+
+            // Pause the program execution
+            Console.ReadLine();
+        }
+    }
+}
