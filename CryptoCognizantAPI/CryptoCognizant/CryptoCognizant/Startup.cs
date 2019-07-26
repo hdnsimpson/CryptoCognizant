38beef82bccfed248485b36aabad9efe7a846df4
Hayden <hdnsimpson@gmail.com>
2019-07-26 12:36:54 +1200

    Updated method to configure http request pipeline

--- CryptoCognizantAPI/CryptoCognizant/CryptoCognizant/Startup.cs
+++ CryptoCognizantAPI/CryptoCognizant/CryptoCognizant/Startup.cs
@@ -51,6 +51,17 @@ namespace CryptoCognizant
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CryptoCognizantAPI V1");
+                c.RoutePrefix = string.Empty; // launch swagger from root
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
