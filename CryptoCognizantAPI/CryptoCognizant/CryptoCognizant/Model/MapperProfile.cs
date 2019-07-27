d9d19d717ff347ec333b96094a8a13c709bcdc0f
Hayden <hdnsimpson@gmail.com>
2019-07-27 19:49:54 +1200

    Created MapperProfile for coin DTO mapping

---
+++ CryptoCognizantAPI/CryptoCognizant/CryptoCognizant/Model/MapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace CryptoCognizant.Model
+{
+    public class MapperProfile: Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Coin, CoinDTO>();
+            CreateMap<CoinDTO, Coin>();
+        }
+    }
+}
