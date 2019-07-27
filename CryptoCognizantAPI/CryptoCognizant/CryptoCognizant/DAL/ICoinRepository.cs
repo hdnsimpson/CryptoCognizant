efb23b5da8f3fc6efa546a58c63831f3f821a72c
Hayden <hdnsimpson@gmail.com>
2019-07-27 19:27:29 +1200

    Added ICoinRepository and the DAL folder

---
+++ CryptoCognizantAPI/CryptoCognizant/CryptoCognizant/DAL/ICoinRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using CryptoCognizant.Model;
+
+namespace CryptoCognizant.DAL
+{
+    public interface ICoinRepository : IDisposable
+    {
+        IEnumerable<Coin> GetCoins();
+        Coin GetCoinByID(int CoinId);
+        void InsertCoin(Coin coin);
+        void DeleteCoin(int CoinId);
+        void UpdateCoin(Coin coin);
+        void Save();
+    }
+}
